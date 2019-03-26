a9d886e54352f5062ff406d05cd862db29d361a2
Zoljargal <zol.fallows@hotmail.com>
2019-03-26 21:14:04 +0000

    updated config,js

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DB_URL || 'postgresql://zol@localhost/forum',
-  JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+  JWT_SECRET: process.env.JWT_SECRET || 'my-own-special-jwt-secret',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3600s',
 };
